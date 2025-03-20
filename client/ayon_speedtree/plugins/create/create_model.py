c728332ca669d157ff13dc7b58e6f69e4a137cda
Kayla Man <moonyuet2008@hotmail.com>
2025-03-20 17:59:09 +0800

    add Model Creator

---
+++ client/ayon_speedtree/plugins/create/create_model.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Creator plugin for model."""
+from ayon_speedtree.api import plugin
+
+
+class CreateModel(plugin.SpeedTreeCreator):
+    """Creator plugin for Model."""
+    identifier = "io.ayon.creators.speedtree.model"
+    label = "Model"
+    product_type = "model"
+    icon = "cube"
